78a0bee78f254a36a6c96fc05502bd9c7640c008
HashirBinKamran <157620848+HashirBinKamran@users.noreply.github.com>
2024-01-29 07:49:41 -0800

    Add files via upload

---
+++ If Statement.py
@@ -0,0 +1,5 @@
+#Sample Python Program to Understand the if statement
+num = int(input("Enter The Number"))
+#we are taking an integer num and taking dynhamically
+if num%2==0:
+    print("The Given Number is an even Number")
